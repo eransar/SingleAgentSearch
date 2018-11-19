8281aa41039dde2968d8d9774f2491e557997a2a
idanf <idanforu@gmail.com>
2018-11-19 12:29:46 +0200

    PUS

--- src/PureHeuristicSearch.java
+++ src/PureHeuristicSearch.java
@@ -1,6 +1,10 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class PureHeuristicSearch  extends ASearch
 {
+	PriorityQueue<HeuristicSearchNode> open;
+	PriorityQueue<HeuristicSearchNode> close;
 	// Define lists here ...
 	
 	@Override
@@ -22,7 +26,32 @@ public class PureHeuristicSearch  extends ASearch
 	@Override
 	public void initLists() 
 	{
-
+		open = new PriorityQueue<HeuristicSearchNode>( new Comparator<ASearchNode>() {
+			public int compare(ASearchNode n1, ASearchNode n2) {
+				// compare n1 and n2
+				if(n1.getG() > n2.getG()){
+					return 1;
+				}
+				else if(n1.getG()==n2.getG()){
+					return 0;
+				}
+				else{
+					return -1;
+				}
+			}});
+		close = new PriorityQueue<HeuristicSearchNode>( new Comparator<ASearchNode>() {
+			public int compare(ASearchNode n1, ASearchNode n2) {
+				// compare n1 and n2
+				if(n1.getG() > n2.getG()){
+					return 1;
+				}
+				else if(n1.getG()==n2.getG()){
+					return 0;
+				}
+				else{
+					return -1;
+				}
+			}});
 	}
 
 	@Override
@@ -31,6 +60,8 @@ public class PureHeuristicSearch  extends ASearch
 		ASearchNode node
 	) 
 	{
+		if(open.contains( node))
+			return node;
 		return null;
 	}
 
@@ -38,8 +69,9 @@ public class PureHeuristicSearch  extends ASearch
 	public boolean isOpen
 	(
 		ASearchNode node
-	) 
-	{
+	) {
+		if (open.contains(node))
+			return true;
 		return false;
 	}
 	
@@ -49,6 +81,8 @@ public class PureHeuristicSearch  extends ASearch
 		ASearchNode node
 	) 
 	{
+		if (close.contains(node))
+			return true;
 		return false;
 	}
 
@@ -60,7 +94,7 @@ public class PureHeuristicSearch  extends ASearch
 		ASearchNode node
 	) 
 	{
-
+		open.add((HeuristicSearchNode)node);
 	}
 
 	@Override
@@ -69,19 +103,19 @@ public class PureHeuristicSearch  extends ASearch
 		ASearchNode node
 	) 
 	{
-
+		close.add((HeuristicSearchNode)node);
 	}
 
 	@Override
 	public int openSize() 
 	{
-		return 0;
+		return open.size();
 	}
 
 	@Override
 	public ASearchNode getBest() 
 	{
-		return null;
+		return open.remove();
 	}
 
 }
--- src/TilePuzzleHeuristic.java
+++ src/TilePuzzleHeuristic.java
@@ -1,14 +1,44 @@
 
-public class TilePuzzleHeuristic implements IHeuristic
-{
+public class TilePuzzleHeuristic implements IHeuristic {
 
 	@Override
 	public double getHeuristic
-	(
-		IProblemState problemState
-	) 
-	{
-		return 0;
+			(
+					IProblemState problemState
+			) {
+		int x=0;
+		int y=0;
+		if (problemState instanceof TilePuzzleState) {
+			int[][] ints1 = ((TilePuzzleState) problemState)._tilePuzzle;
+			int[][] int2 = goAL(ints1.length);
+			for (int i = 0; i < ints1.length; i++) {
+				for (int k = 0; k < ints1.length; k++) {
+					if (ints1[i][k] != int2[i][k]) {
+						for (int p = 0; i < int2.length; i++) {
+							for (int r = 0; k < int2.length; k++) {
+								if (ints1[i][k] == int2[p][r]) {
+									x= x+Math.abs(k-r);
+									y= y+Math.abs(i-p);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return x+y;
 	}
-	
+
+	private int[][] goAL (int size){
+		int j = 1;
+		int[][] ints = new int[size][size];
+		for (int i = 0; i < size; i++) {
+			for (int k = 0; k < size; k++) {
+				ints[i][k] = j;
+				j++;
+			}
+		}
+		ints[size - 1][size - 1] = 0;
+		return ints;
+	}
 }
